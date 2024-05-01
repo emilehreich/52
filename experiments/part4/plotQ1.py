ccbc84d46b0f1589173a937bc98f29f54359b6b2
emarty <emile.marty@epfl.ch>
2024-05-01 19:24:02 +0200

    Q1 data and plot

--- experiments/part4/plotQ1.py
+++ experiments/part4/plotQ1.py
@@ -3,7 +3,9 @@ import matplotlib.pyplot as plt
 import glob
 
 # Get a list of all txt files in the current directory
-files = ['memcachedT1C1.txt']
+files = glob.glob('memcachedT?C?.txt')
+# files.append('memcachedT16C1.txt')
+# files.append('memcachedT16C2.txt')
 
 # create a new figure
 plt.figure(figsize=(12, 9))
@@ -11,8 +13,13 @@ plt.figure(figsize=(12, 9))
 # Define column headers
 headers = ['type', 'avg', 'std', 'min', 'p5', 'p10', 'p50', 'p67', 'p75', 'p80', 'p85', 'p90', 'p95', 'p99', 'p999', 'p9999', 'QPS', 'target', 'ts_start', 'ts_end'] 
 
+# Okabe-Ito color palette                 
+#           black     orange     skyblue  bluishgreen   yellow     blue    vermillion  red-purple    gray
+colors = ["#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#999999"] 
+markers = ["v", "s", "^", "o", "o", "o"]
+
 # Loop through the files list and plot the data from each file
-for file in files:
+for index, file in enumerate(files):
     # Load data from file into a pandas DataFrame
     print(file)
     df = pd.read_csv(file, delim_whitespace=True, comment='#', names=headers)
@@ -26,21 +33,27 @@ for file in files:
     result_error = grouped.sem()
 
     # Plot QPS vs average p95, with error bars for stddev (QPS and p95)
-    plt.errorbar(result_mean['QPS'], result_mean['p95'], xerr=result_error['QPS'], yerr=result_error['p95'], label=file[0:-4], linewidth=0.01)
+    plt.errorbar(result_mean['QPS'], result_mean['p95'], xerr=result_error['QPS'], yerr=result_error['p95'], capsize=2,
+                 label=file[9:-4], linestyle='--', linewidth=1.0, color=colors[index+3], marker=markers[index], markersize=8, fillstyle='none')
+
+plt.plot(range(130000), [1]*130000, linestyle='--', linewidth=2.0, color=colors[-1])
 
 # Set the size of the text
-text_size = 16.5
+text_size = 12
 
 # labeling axes
-plt.xlabel('Average Queries per Second (1/s)', fontsize=text_size)
-plt.ylabel('Average P95 Latency (ms)', fontsize=text_size)
+plt.xlabel('Average Queries per Second $(s^{-1})$', fontsize=text_size)
+plt.ylabel('Average P95 Latency $(ms)$', fontsize=text_size)
+
+# Add a grid
+plt.grid()
 
 # Add legend
-plt.legend(fontsize=text_size)
+plt.legend(fontsize=text_size, markerscale=0.75)
 
 # Set the limits of the axes
 plt.xlim(0, 130e3)
-plt.ylim(0, 8)
+plt.ylim(0, 2)
 plt.xticks([0,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,110000,120000], 
            ["0", "10K", "20K", "30K", "40K", "50K", "60K", "70K", "80K", "90K", "100K", "110K", "120K"])
 
@@ -48,4 +61,4 @@ plt.xticks([0,10000,20000,30000,40000,50000,60000,70000,80000,90000,100000,11000
 # plt.title('Latency vs QPS over various interference types', fontsize=text_size)
 
 # Display the plot
-plt.savefig("part1_basic.pdf", bbox_inches='tight')
+plt.savefig("part4_q1.pdf", bbox_inches='tight')
