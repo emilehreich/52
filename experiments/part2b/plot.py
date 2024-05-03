82a6d1b9d22cc63f358c6ebd95f7b69b762d41dd
joshua.freeman@epfl.ch <joshua.freeman@epfl.ch>
2024-05-03 11:12:54 +0200

    wip

--- experiments/part2b/plot.py
+++ experiments/part2b/plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 import re
+import numpy as np
 
 def get_avg_real_time(file_name):
     with open(file_name, 'r') as f:
@@ -19,16 +20,26 @@ def plot_graph(experiment):
 
     speedup = [times[0]/time for time in times]
 
-    plt.plot(threads, speedup, marker='o', label=experiment, linewidth=0.01)
+    plt.plot(threads, speedup, marker='o', markersize=1.5, label=experiment, linewidth=0.01)
 
 experiments = ["dedup", "blackscholes", "canneal", "ferret", "freqmine", "radix", "vips"]
 
 for experiment in experiments:
     plot_graph(experiment)
 
+threads = range(1,9)
+# Amdahl's law
+P = 0.875
+threads = np.linspace(1, 8, 500) 
+amdahl_speedup = [1 / ((1-P) + P/thread) for thread in threads]
+plt.plot(threads, amdahl_speedup, marker=None, label='Amdahl\'s law (P=0.9)', linestyle= ':', linewidth=1)
+    
+plt.plot(threads, threads, marker=None, label='Linear Speedup', linestyle=':', linewidth=1)
+
+
 plt.xlabel('Number of Threads')
 plt.ylabel('Speedup')
-plt.title('Speedup vs Number of Threads')
-plt.legend()
+plt.legend(fontsize=9)
+plt.ylim(0.75,7.5)
 plt.grid(True)
 plt.savefig("part2b.pdf")
