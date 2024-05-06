49a13af4477e4f91dceee5eda70d64dc7e2195fd
Emile Janho Dit Hreich <emile.jd.hreich@gmail.com>
2024-05-06 08:51:09 +0200

    set CPU configuration

--- dynamic-scheduler/scripts/utils/batch_applications.py
+++ dynamic-scheduler/scripts/utils/batch_applications.py
@@ -1,37 +1,37 @@
 from enum import Enum
 
 class BatchApplication(Enum):
-    DEDUP = ("0,1,2,3",
+    DEDUP = ("2,3",
          "dedup",
          "anakli/cca:parsec_dedup",
          "./run -a run -S parsec -p dedup -i native -n 2")
     
-    BLACKSHOLES = ("0,1,2,3",
+    BLACKSHOLES = ("2,3",
                 "blackscholes",
                 "anakli/cca:parsec_blackscholes",
                 "./run -a run -S parsec -p blackscholes -i native -n 2")
     
-    CANNEAL = ("0,1,2,3",
+    CANNEAL = ("2,3",
            "canneal",
            "anakli/cca:parsec_canneal",
            "./run -a run -S parsec -p canneal -i native -n 2")
     
-    FREQMINE = ("0,1,2,3",
+    FREQMINE = ("1,2,3",
             "freqmine",
             "anakli/cca:parsec_freqmine",
             "./run -a run -S parsec -p freqmine -i native -n 3")
     
-    FERRET = ("0,1,2,3",
+    FERRET = ("2,3",
           "ferret",
           "anakli/cca:parsec_ferret",
           "./run -a run -S parsec -p ferret -i native -n 2")
     
-    VIPS = ("0,1,2,3",
+    VIPS = ("3",
         "vips",
         "anakli/cca:parsec_vips",
         "./run -a run -S parsec -p vips -i native -n 1")
     
-    RADIX = ("0,1,2,3",
+    RADIX = ("1,2,3",
             "radix",
             "anakli/cca:parsec_radix",
             "./run -a run -S parsec -p radix -i native -n 3")
